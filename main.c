7c2ea1edbb13601f398b5929f32bfb51503b9aa8
Thierry Seegers <surfintheusa@yahoo.com>
2021-01-07 18:23:46 +0100

    Mention total number of tests. Adjust test column width. Change int to long.

--- main.c
+++ main.c
@@ -2,18 +2,18 @@
 
 #include <stddef.h>
 
-int const tests_total = 2;
-int const test_column_width = 40;
+int const tests_total = 14;
+int const test_column_width = 60;
 
-int maximum_power(int panels[], size_t const size_panels)
+long maximum_power(long panels[], size_t const size_panels)
 {
     return 0;
 }
 
 int main()
 {
-    TEST(maximum_power((int[]){2, 0, 2, 2, 0}, 5) == 8);
-    TEST(maximum_power((int[]){-2, -3, 4, -5}, 5) == 60);
+    TEST(maximum_power((long[]){2, 0, 2, 2, 0}, 5) == 8);
+    TEST(maximum_power((long[]){-2, -3, 4, -5}, 5) == 60);
 
     // ???
 
